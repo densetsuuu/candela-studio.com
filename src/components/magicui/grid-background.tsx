6da9c637fbf12b80cdbd656ab837b78ba42ac1c5
densetsuuu <raph.alarcon1@gmail.com>
2025-06-20 09:20:43 +0200

    fix

--- src/components/magicui/grid-background.tsx
+++ src/components/magicui/grid-background.tsx
@@ -5,10 +5,11 @@ export default function GridBackground() {
         <div className="absolute inset-0 -z-10 pointer-events-none" aria-hidden="true">
             {/* Gradient blob */}
             <div
-                className="absolute top-0 left-0 w-[400px] h-[400px] bg-sky-400 opacity-20 blur-[150px] rounded-full"/>
+                className="absolute top-0 left-0 w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] bg-sky-400 opacity-20 blur-[120px] rounded-full"
+            />
             <div
-                className="absolute top-1/10 right-0 w-[500px] h-[500px] bg-purple-400 opacity-25 blur-[160px] rounded-full"/>
-
+                className="absolute top-[10%] right-0 w-[350px] h-[350px] sm:w-[500px] sm:h-[500px] bg-purple-400 opacity-25 blur-[130px] rounded-full"
+            />
             {/* Grid pattern */}
             <div
                 className="w-full h-full"
