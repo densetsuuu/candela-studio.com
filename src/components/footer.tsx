0f3d34ec5e6507d26740c384aff5e80892a92ab4
densetsuuu <raph.alarcon1@gmail.com>
2025-06-26 10:48:12 +0200

    fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,35 +1,38 @@
 import Link from "next/link";
-import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import {GitHubLogoIcon, LinkedInLogoIcon} from "@radix-ui/react-icons";
 
 export function Footer() {
-	return (
-		<footer className="w-full grid place-items-center mt-20 xl:mt-0 text-muted-foreground" id="page-footer">
-			<div className="px-10 flex flex-col gap-4 md:gap-0 md:flex-row items-center justify-between w-full h-20">
-				<p className="text-sm dark:text-gray-400">
-					© {new Date().getFullYear()} Candela Studio. Tous droits réservés.
-				</p>
-				<div
-					className="flex flex-row gap-2">
-					<Link href="https://www.linkedin.com/in/raphael-alarcon/" className="group" target="_blank"
-								aria-label="Link to LinkedIn profile">
-						<LinkedInLogoIcon
-							className="size-5 group-hover:text-gray-700"/>
-					</Link>
-					<Link href="https://github.com/densetsuuu" className="group" target="_blank"
-								aria-label="Link to Github profile">
-						<GitHubLogoIcon className="size-5 group-hover:text-gray-700"/>
-					</Link>
-				</div>
+    return (
+        <footer
+            className="w-full grid place-items-center mt-20 xl:mt-0 text-muted-foreground bg-white border-t dark:bg-gray-950 dark:text-gray-400"
+            id="page-footer">
+            <div
+                className="px-10 py-4 flex flex-col gap-4 md:gap-0 md:flex-row items-center justify-between w-full h-20">
+                <p className="text-sm dark:text-gray-400">
+                    © {new Date().getFullYear()} Candela Studio. Tous droits réservés.
+                </p>
+                <div
+                    className="flex flex-row gap-2">
+                    <Link href="https://www.linkedin.com/in/raphael-alarcon/" className="group" target="_blank"
+                          aria-label="Link to LinkedIn profile">
+                        <LinkedInLogoIcon
+                            className="size-5 group-hover:text-gray-700"/>
+                    </Link>
+                    <Link href="https://github.com/densetsuuu" className="group" target="_blank"
+                          aria-label="Link to Github profile">
+                        <GitHubLogoIcon className="size-5 group-hover:text-gray-700"/>
+                    </Link>
+                </div>
 
-				<div className="flex flex-col md:flex-row gap-4 items-center justify-center pb-4 md:pb-0">
-					<Link className="text-xs dark:text-gray-400" href="/privacy-policy">
-						Politique de confidentialité
-					</Link>
-					<Link className="text-xs dark:text-gray-400" href="/terms-of-service">
-						Conditions d&apos;utilisation
-					</Link>
-				</div>
-			</div>
-		</footer>
-	)
+                <div className="flex flex-col md:flex-row gap-4 items-center justify-center pb-4 md:pb-0">
+                    <Link className="text-xs dark:text-gray-400" href="/privacy-policy">
+                        Politique de confidentialité
+                    </Link>
+                    <Link className="text-xs dark:text-gray-400" href="/terms-of-service">
+                        Conditions d&apos;utilisation
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    )
 }
--- src/components/sections/arguments-section.tsx
+++ src/components/sections/arguments-section.tsx
@@ -6,7 +6,7 @@ import ArgumentsList from "@components/sections/arguments/arguments-list";
 export function ArgumentsSection() {
 
     return (
-        <div className="w-full bg-background/10 py-24 px-4 sm:px-6 lg:px-8">
+        <div className="w-full py-24 px-4 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-7xl text-center flex flex-col items-center justify-center gap-4">
                 <NeumorphEyebrow intent="primary" className="text-2xl">
                     Pourquoi choisir Candela Studio ?
